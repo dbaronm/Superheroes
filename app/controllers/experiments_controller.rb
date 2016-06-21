b13a43aa700150c4915fb0c74579c5352ccba203
David <david.baron@softtek.com>
2016-06-20 22:01:07 -0500

    added boostrap layout to page

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,10 +1,11 @@
 class ExperimentsController < ApplicationController
-  def page1
-  end
-
-  def page2
-  end
-
-  def page3
-  end
+ 	def page1
+		render layout: 'bootstrap'
+	end
+	def page2	
+		render layout: 'bootstrap'
+	end
+	def pag3
+		render layout: 'bootstrap'
+	end
 end
